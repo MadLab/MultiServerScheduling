c96df952e26ca3537cf3e4cd4be0ea774bc6f8ef
Nick Ashley <nick@madlab.com>
2016-09-27 11:43:42 -0500

    Updating abandoned task logic. Now all tasks are added to cache with a 1 hour expiration, so abandoned tasks will be cleared automatically.

--- src/Event.php
+++ src/Event.php
@@ -52,7 +52,7 @@ class Event extends NativeEvent
     public function withoutOverlappingCache()
     {
         return $this->skip(function () {
-            return $this->skipMultiserver();
+            return $this->skipCache();
         });
     }
 
@@ -65,19 +65,11 @@ class Event extends NativeEvent
         $this->key = md5($this->expression.$this->command);
 
 
-        // See if we can find this task in the cache
-        if($eventInfo = Cache::get($this->key)) {
-            // Task found. Is it abandoned?
-            $startDate = $eventInfo->get('startDate');
-            $now = \Carbon\Caron::now();
-            if(empty($eventInfo->get('endDate')) && $now->diffInHours($startDate, true) >=1){
-                //Task has been abandonded. Delete it.
-                Cache::forget($this->key);
-            }
-            else{
-                //Someone else has the lock.
-                return true;
-            }
+        // Check to see if the mutex is found in the cache
+        if(Cache::has($this->key)){
+            //Someone else has the lock.
+            echo 'lock:' . $this->command . "\n";
+            return true;
         }
 
         // Attempt to acquire the lock
@@ -87,8 +79,10 @@ class Event extends NativeEvent
         $eventInfo->put('lock',$this->serverId);
         $eventInfo->put('startDate',\Carbon\Carbon::now());
 
-        // If the mutex already exists in the cache, the above 'add' will return false
-        if(Cache::add($this->key, $eventInfo)){
+        // If the mutex already exists in the cache, this 'add' will return false.
+        // If it succeeds, we set a max execution time of 1 hour. At that point, we
+        // consider the task 'abandoned' and the lock will be released.
+        if(Cache::add($this->key, $eventInfo, \Carbon\Carbon::now()->addHour(1))){
             //we got the lock
             return false;
         }
@@ -103,10 +97,9 @@ class Event extends NativeEvent
      */
     public function clearCache()
     {
-        // Set the lock to expire in 10 seconds
+        // The task finished, so Set the lock to expire in 10 seconds
         if ($this->serverId) {
-            $eventInfo = Cache::get($this->key);
-            $eventInfo->put('endDate',\Carbon\Carbon::now());
+            $eventInfo = Cache::pull($this->key);
             Cache::put($this->key, $eventInfo, \Carbon\Carbon::now()->addSeconds(10));
         }
     }
